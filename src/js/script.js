260a99798d97d87a95405b2b42354e74e280f954
asmaamostafa74 <asmaaelbanaa123@gmail.com>
2024-10-26 14:27:46 +0300

    Add Sidebar and responsive

--- src/js/script.js
+++ src/js/script.js
@@ -1,12 +1,27 @@
-// Load Navbar
+  // Load Navbar
 document.addEventListener("DOMContentLoaded", function() {
     fetch('offer&nav.html')
         .then(response => response.text())
         .then(data => {
             document.getElementById('navbar').innerHTML = data;
+            initializeSidebar();
         })
         .catch(error => console.log('Error loading the navbar:', error));
 });
+
+function initializeSidebar() {
+    const sidebarToggle = document.getElementById("sidebarToggle");
+    const closeSidebar = document.getElementById("closeSidebar");
+    const sidebar = document.getElementById("sidebar");
+
+    sidebarToggle.addEventListener("click", function () {
+        sidebar.classList.toggle("-translate-x-full");
+    });
+
+    closeSidebar.addEventListener("click", function () {
+        sidebar.classList.add("-translate-x-full");
+    });
+}
 
 // Load Footer
 document.addEventListener("DOMContentLoaded", function() {
